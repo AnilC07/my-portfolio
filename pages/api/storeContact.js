25756fd66a203cf9d3858d7ab7678c58e12b9b2d
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-13 11:52:56 +0200

    try to fix soreContact a la DB

--- pages/api/storeContact.js
+++ pages/api/storeContact.js
@@ -1,20 +1,25 @@
-const { default: connectToDB } = require("@/lib/db/connectToDB");
+import connectToDB from "@/lib/db/connectToDB";
 
 const storeUserData = async (req, res) => {
   const { name, email } = req.body;
+  console.log(req.body)
+  console.log(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.u3vdkvu.mongodb.net/${process.env.MONGODB_DATABASE_NAME}`)
 
   let client;
   try {
     client = await connectToDB();
+    console.log("storeContact " + client);
   } catch (error) {
+    console.log(error.message)
     res.status(500).json({ status: "Connection à la DB échouée" });
-    client.close();
     return;
   }
 
   try {
-    const db = await client
-      .db()
+    const db = client.db();
+    console.log(db)
+
+    const result = await db
       .collection("visiteur")
       .insertOne({ name: name, email: email });
     newMessage.id = result.insertedId;
@@ -24,12 +29,10 @@ const storeUserData = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 
-  client.close();
-
-  res
-    .status(201)
-    .json({
-      status: "success",
-      message: "Les informations ont bien été enrengistrer",
-    });
+  res.status(201).json({
+    status: "success",
+    message: "Les informations ont bien été enrengistrer",
+  });
 };
+
+export default storeUserData;
