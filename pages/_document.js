561eb2d738b6649f5d61583900dddae1d69779b1
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-13 21:25:43 +0200

    màj de la balise title

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-    <title>Bienvenue</title>
+    <title>Anil Celik - Portfolio</title>
     <meta name="viewport" content='width=device-width, initial-scale=1' />
-    <meta name='description' content="Jeune developpeur, je tiens ce portfolio afin de montrer mes connaissances et mes compétences auprès des recruteurs et futures client qui souhaiteraient une prestation. Mon domaine d'expertise est principalement le language JavaScript. Mes connaissances au frameworks Next.js me permettent de pouvoir realiser une application complete ou un site web de maniere professionnelles. Pour toute demande de prestation, merci de bien vouloir prendre contact via la page 'contact'." />
+    <meta name='description' content="Je me prénomme Anil CELIK et je tiens ce portfolio afin de montrer mes connaissances et mes compétences auprès des recruteurs et futures client qui souhaiteraient une prestation. Mon domaine d'expertise est principalement le language JavaScript. Mes connaissances au frameworks Next.js me permettent de pouvoir realiser une application complete ou un site web de maniere professionnelles. Pour toute demande de prestation, merci de bien vouloir prendre contact via la page 'contact'." />
       <Head />
       <body>
         <Main />
