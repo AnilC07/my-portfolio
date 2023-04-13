d42f382e80e7e7f3fa34e7fce283374ddd114d8a
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-13 22:38:03 +0200

    edit css

---
+++ components/sections/Abouts.js
@@ -0,0 +1,38 @@
+import classes from "./About.module.css";
+
+function About(props) {
+  return (
+    <section className={classes.about}>
+      <div className="section-title">
+        <h2>Qui suis-je ?</h2>
+      </div>
+      <div className={classes.text}>
+        <p>
+          Tout d'abord, merci d'avoir pris le temps de visiter ce portfolio.
+          <br></br>
+          Je me prénomme Anil, j'ai 31 ans et c'est que récemment que j'ai
+          découvert dans le développement web.
+          <br></br>
+          De formation en électronique, j'ai travaillé un peu plus de trois ans
+          dans ce secteur d'activité.<br></br>
+          Entre deux missions d'intérim, j'ai pris la décision de me former à un
+          autre métier et ayant beaucoup d'intérêt pour le numérique et pour le
+          web, je me suis intéressé au développement de celui-ci.
+          <br></br>
+          Commencé en autodidacte, j'ai appris les bases du langage HTML, CSS et
+          JavaScript puis à la librairie React. C'est naturellement que j'ai
+          suivi une formation de développeur web et web mobile durant l'année
+          2022. C'est suite à cette formation et au stage que j'ai pu effectuer
+          au sein de l'association gaea21 (
+          <a href="https://gaea21.org" target="_blank">
+            gaea21.org
+          </a>
+          ) que j'ai pris la décision de devenir{" "}
+          <b>developpeur full-stack JS.</b>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
