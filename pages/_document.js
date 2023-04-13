01ed7c5f784055611a5626c6ce007648712322a0
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-13 21:23:10 +0200

    ajout d'une metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
     <title>Bienvenue</title>
+    <meta name="viewport" content='width=device-width, initial-scale=1' />
+    <meta name='description' content="Jeune developpeur, je tiens ce portfolio afin de montrer mes connaissances et mes compétences auprès des recruteurs et futures client qui souhaiteraient une prestation. Mon domaine d'expertise est principalement le language JavaScript. Mes connaissances au frameworks Next.js me permettent de pouvoir realiser une application complete ou un site web de maniere professionnelles. Pour toute demande de prestation, merci de bien vouloir prendre contact via la page 'contact'." />
       <Head />
       <body>
         <Main />
