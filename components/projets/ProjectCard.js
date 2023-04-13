19975622486d42707ba98387bdc2dff6714202fe
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-13 22:38:52 +0200

    preparation du composant

--- components/projets/ProjectCard.js
+++ components/projets/ProjectCard.js
@@ -3,17 +3,18 @@ import classes from "./ProjectCard.module.css";
 import Link from "next/link";
 
 function Card(props) {
+  const {titre, image, description} = props
   return (
     
     <div className={classes.card}>
     <Link href="/">
       <div className={classes.vignette}>
-        <Image src="/images/trillo.png" alt="" width="170" height="130" />
+        <Image src={image} alt={`vignette du projet ${titre}`} width="170" height="130" />
       </div>
       </Link>
       <div className={classes.contenue}>
-        <h3>Titre</h3>
-        <p>description</p>
+        <h3>{titre}</h3>
+        <p>{description}</p>
       </div>
       
     </div>
--- components/projets/ProjectContainer.js
+++ components/projets/ProjectContainer.js
@@ -1,40 +1,31 @@
 import Card from "./ProjectCard";
 import classes from "./ProjectContainer.module.css";
 
-function ProjectContainer({ children }) {
+function ProjectContainer(props) {
   return (
     <div className={classes.container}>
       <div className={classes.header}>
         <h2 className={classes.competence}>React</h2>
       </div>
       <div className={classes.projets}>
-        <Card />
-        <Card />
+        <Card titre="" image="" description="" />
       </div>
       <div className={classes.header}>
         <h2 className={classes.competence}>Next.js</h2>
       </div>
-      <div className={classes.projets}>
-        <Card />
-      </div>
+      <div className={classes.projets}></div>
       <div className={classes.header}>
         <h2 className={classes.competence}>Node.js</h2>
       </div>
-      <div className={classes.projets}>
-        <Card />
-      </div>
+      <div className={classes.projets}></div>
       <div className={classes.header}>
         <h2 className={classes.competence}>React</h2>
       </div>
-      <div className={classes.projets}>
-        <Card />
-      </div>
+      <div className={classes.projets}></div>
       <div className={classes.header}>
         <h2 className={classes.competence}>API</h2>
       </div>
-      <div className={classes.projets}>
-        <Card />
-      </div>
+      <div className={classes.projets}></div>
     </div>
   );
 }
