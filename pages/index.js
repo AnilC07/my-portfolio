12ffe715c1c778b10714e5d9d8f599d6d3087ae9
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-15 02:20:17 +0200

    add get staticprops function

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import classes from "../styles/Home.module.css";
 import About from "@/components/sections/Abouts";
+import ProjectContainer from "@/components/projets/ProjectContainer";
 
 export default function Home(props) {
   return (
@@ -9,6 +10,20 @@ export default function Home(props) {
         <title>Accueil</title>
       </Head>
       <About />
+      <ProjectContainer />
     </>
   );
+}
+
+
+export async function getStaticProps(){
+
+
+return {
+  props: {
+    status:"message",
+    data : 'fetchedData'   
+  },
+  revalidate:60
+ }
 }
