be1e65982dde7744c4955e2ffac5ca35fd231789
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-18 01:33:39 +0200

    Edit component

--- components/projets/ProjectCard.js
+++ components/projets/ProjectCard.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Card(props) {
   // console.log(props);
-  const { titre, image, description,id } = props;
+  const { titre, image, description, id } = props;
   // console.log(`/images/projets/${titre}/${image}`);
   return (
     <div className={classes.card}>
@@ -13,7 +13,7 @@ function Card(props) {
           <Image
             src={`/images/projets/${titre}/${image}`}
             alt={`vignette du projet ${titre}`}
-            width="170"
+            width="200"
             height="130"
           />
         </div>
