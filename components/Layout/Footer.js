371cfacfa62156772ceb544ff0c6c32041cd5e6a
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-04-18 02:52:23 +0200

    edit footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -7,13 +7,18 @@ function Footer(props) {
   return (
     <footer className={classes.footer}>
       <div className={classes.icons}>
+      <div className={classes['contact-action']}>
         <Link href="/contact">
           <GrMail />
+          <p>Envoyer un mail</p>
         </Link>
-        <a href="https://www.linkedin.com/in/anil-celik-16067011a" target='_blank'>
+        </div>
+<div className={classes['contact-action']}>  <a href="https://www.linkedin.com/in/anil-celik-16067011a" target='_blank'>
 
           <GrLinkedin />
 </a>
+<p>Profil LinkedIn</p>
+</div>
       </div>
 
       <div className={classes.copyright}>
